camptocamp/devops-stack-module-dex

commit e0ea203285ec2ac77d63e90e8a707254a851e284

feat!: add support to oboukili/argocd v5 (#11)
* feat!: add support to oboukili/argocd v5

* docs(terraform-docs): generate docs and write to README.adoc

---------

Co-authored-by: hbollon <hbollon@users.noreply.github.com>

main.tf
@@ -63,7 +63,20 @@ resource "argocd_application" "this" {
     }
 
     sync_policy {
-      automated = var.app_autosync 
+      automated {
+        prune       = var.app_autosync.prune
+        self_heal   = var.app_autosync.self_heal
+        allow_empty = var.app_autosync.allow_empty
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
 
       sync_options = [
         "CreateNamespace=true"

terraform.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     argocd = {
-      source = "oboukili/argocd"
+      source  = "oboukili/argocd"
+      version = ">= 5"
     }
 
     utils = {

commit ad9d93f6ae0d9ed5be97ce2791e0785213af568e

fix: readd support to deactivate auto-sync which was broken by #11

main.tf
@@ -63,10 +63,13 @@ resource "argocd_application" "this" {
     }
 
     sync_policy {
-      automated {
-        prune       = var.app_autosync.prune
-        self_heal   = var.app_autosync.self_heal
-        allow_empty = var.app_autosync.allow_empty
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
       }
 
       retry {